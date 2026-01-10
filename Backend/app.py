78b0e3779df647117119d0271ff830cbb156df17
Mary Ann <maryannliu01@gmail.com>
2026-01-10 15:09:38 +1000

    Added background check page and privacy notice html. Changed app.py accordingly

--- Backend/app.py
+++ Backend/app.py
@@ -8,5 +8,9 @@ app = Flask(__name__, template_folder="../Frontend/pages")
 def q_privacyNotice():
     return render_template("q_privacyNotice.html")
 
+@app.get("/backgroundCheck")
+def q_backgroundCheck():
+    return render_template("q_backgroundCheck.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
