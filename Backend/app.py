37401103503b44d2b995476419f593ab062b070b
Jia <jechin17@gmail.com>
2026-01-21 21:48:48 +1000

    update data asset 1 landing

--- Backend/app.py
+++ Backend/app.py
@@ -22,5 +22,20 @@ def backgroundcheck():
 def DA1():
     return render_template('pages/DataAsset1.html')
 
+@app.route('/personal-data-asset')
+def personal_data_asset():
+    """Personal data asset intro page"""
+    return render_template('pages/personalDataAsset.html')
+
+@app.route('/personal-data-asset-questionnaire')
+def personal_data_asset_2():
+    """Personal data asset questionnaire"""
+    return render_template('pages/personalDataAsset2.html')
+
+@app.route('/health-data-asset')
+def health_data_asset_p2():
+    """Health data asset page"""
+    return render_template('pages/healthDataAssetP2.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
